018d7da15772268c1810b81d1808385f9c5d241b
Sushant Mishra <31570651+m-sushant@users.noreply.github.com>
2024-11-30 07:30:14 +0000

    Added the code to handle negative numbers

--- kata.js
+++ kata.js
@@ -3,10 +3,15 @@ const add = (numbers) => {
     return 0;
   }
   let numSplitter = /,|\n/;
-  if(numbers.startsWith('//')) {
+  if (numbers.startsWith('//')) {
     const split = numbers.split('\n');
     numSplitter = new RegExp(split[0].slice(2));
     numbers = split[1];
+  }
+  const convertedStringToNum = numbers.split(numSplitter).map(num => parseInt(num, 10));
+  const negativeNumbers = convertedStringToNum.filter(num => num < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
   }
   return numbers.split(numSplitter).reduce((sum, number) => sum + parseInt(number, 10), 0);
 }
